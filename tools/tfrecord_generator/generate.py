c230f0c6f81a03fd947928cfa2d3ebc6a58f1355
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2018-04-14 17:52:20 -0700

    Add multiple backgrounds and rotations when generating records.

--- tools/tfrecord_generator/generate.py
+++ tools/tfrecord_generator/generate.py
@@ -1,4 +1,3 @@
-#!/bin/python3
 r"""Convert raw Magic: The Gathering data into a set of ".tfrecord" files.
 
 Example usage:
@@ -17,6 +16,7 @@ import json
 import os
 import glob
 import json
+import math
 import numpy as np
 
 import tensorflow as tf
@@ -29,14 +29,16 @@ tf.flags.DEFINE_string('data_dir', './raw_data/',
                        'Training image directory.')
 tf.flags.DEFINE_string('output_dir', '/tmp/', 'Output data directory.')
 tf.flags.DEFINE_boolean('show_debug', False, 'Display a debug image with bounding boxes drawn.')
+tf.flags.DEFINE_string('random_seed', '', 'Seed to supply to the random number generator.')
 
 FLAGS = flags.FLAGS
-MAX_UNDERFITTING_PIXELS = 80
-MAX_COLOR_BALANCE_DELTA = 0.3
-MAX_BRIGHTNESS_DELTA = 0.4
+MAX_UNDERFITTING_PIXELS = 200
+MAX_COLOR_BALANCE_DELTA = 2
+MAX_BRIGHTNESS_DELTA = 2
 TRAINING_SEGMENTS = 2
-BACKGROUND_NAME = "background.png"
+BACKGROUND_NAMES = ["background1.png", "background2.png", "background3.png", "background4.png"]
 NUM_EVAL_EXAMPLES = 10
+MAX_ROTATION_DEGREES = 8
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -44,30 +46,50 @@ def randomise_image(foreground_bytes, background_path):
     im = Image.open(foreground_bytes)
     bg = Image.open(background_path, mode="r")
 
-    oldsize = im.size
+    origsize = im.size
+
+    # Decide on a rotation
+    rotation = random.randint(-MAX_ROTATION_DEGREES, MAX_ROTATION_DEGREES)
 
     # Downscale
-    im.thumbnail((bg.size[0]-random.randint(0, MAX_UNDERFITTING_PIXELS), bg.size[1]-random.randint(0, MAX_UNDERFITTING_PIXELS)), Image.LANCZOS)
+    after_rotation_extra_size = (np.array(get_rotated_bounding_box_size(im.size, rotation))-im.size)
+    im.thumbnail((bg.size[0]-random.randint(0, MAX_UNDERFITTING_PIXELS)-after_rotation_extra_size[0], bg.size[1]-random.randint(0, MAX_UNDERFITTING_PIXELS))-after_rotation_extra_size[1], Image.LANCZOS)
+
+    after_thumbnail_size = np.array(im.size)
+
+    # Rotate
+    im = im.rotate(rotation, Image.BICUBIC, True)
 
     # Position the image randomly
-    margin_x = (bg.size[0]//2)-(im.size[0]//2)
-    margin_y = (bg.size[1]//2)-(im.size[1]//2)
-    transform_x = (random.choice([-1,1]) * random.randint(0,margin_x))
-    transform_y = (random.choice([-1,1]) * random.randint(0,margin_y))
-    bg.paste(im, (margin_x + transform_x,  margin_y + transform_y), im)
+    margin = (np.array(bg.size)//2)-(np.array(im.size)//2)
+    transform = (random.randint(-margin[0],margin[0]), random.randint(-margin[1],margin[1]))
+    bg.paste(im, tuple(margin+transform), im)
 
     # Apply some random image enhancements (color balance, brightness, sharpness, contrast, etc.)
-    im = ImageEnhance.Color(im).enhance((random.random() * MAX_COLOR_BALANCE_DELTA * random.choice([-1,1])) + 1)
-    im = ImageEnhance.Brightness(im).enhance((random.random() * MAX_BRIGHTNESS_DELTA * random.choice([-1,1])) + 1)
+    im = ImageEnhance.Color(im).enhance(random.uniform(-MAX_COLOR_BALANCE_DELTA, MAX_COLOR_BALANCE_DELTA) + 1)
+    im = ImageEnhance.Brightness(im).enhance(random.uniform(-MAX_BRIGHTNESS_DELTA, MAX_BRIGHTNESS_DELTA) + 1)
 
     # bg.size should == (1024, 1024)
-    return bg, (transform_x + margin_x, transform_y + margin_y), (im.size[0]/oldsize[0], im.size[1]/oldsize[1]), oldsize
+    return bg, tuple(margin+transform), after_thumbnail_size / origsize, origsize, rotation, (((np.array(bg.size)//2)-(np.array(after_thumbnail_size)//2))+transform)-(margin+transform)
+
+def rotate_point(point, center, rotation_degrees):
+    r = math.radians(rotation_degrees)
+    return (math.cos(r) * (point[0] - center[0]) + math.sin(r) * (point[1] - center[1]) + center[0],
+    math.sin(r) * -(point[0] - center[0]) + math.cos(r) * (point[1] - center[1]) + center[1])
+
+def get_rotated_bounding_box_size(size, rotation_degrees):
+    if math.fabs(rotation_degrees) != rotation_degrees:
+        rotation_degrees += 90
+        size = size[::-1]
+    r = math.radians(rotation_degrees)
+    return ((math.cos(r)*size[0])+(math.sin(r)*size[1]), (math.cos(r)*size[1])+(math.sin(r)*size[0]))
 
 def create_tf_record_from_annotations(in_dir, in_ids, out_path):
-    background_png_path = os.path.join(in_dir, BACKGROUND_NAME)
     writer = tf.python_io.TFRecordWriter(out_path)
 
     for v in in_ids:
+        background_png_path = os.path.join(in_dir, random.choice(BACKGROUND_NAMES))
+
         json_path = os.path.join(in_dir, v + ".json")
         with tf.gfile.GFile(json_path, "r") as f:
             json_raw = f.read()
@@ -78,15 +100,15 @@ def create_tf_record_from_annotations(in_dir, in_ids, out_path):
         print(card_png_path)
         with tf.gfile.GFile(card_png_path, 'rb') as fid:
             encoded_png = fid.read()
-        randomised_image, card_transform, card_resize_factor, original_size = randomise_image(io.BytesIO(encoded_png), background_png_path)
+        randomised_image, card_transform, thumbnail_resize_factor, card_original_size, rotation, content_transform = randomise_image(io.BytesIO(encoded_png), background_png_path)
         if randomised_image.format != "PNG":
             raise ValueError("Image format not PNG")
 
         annotation["card"] = {
             "startX": 0,
             "startY": 0,
-            "endX": original_size[0],
-            "endY": original_size[1],
+            "endX": card_original_size[0],
+            "endY": card_original_size[1],
         }
 
         startx = []
@@ -99,23 +121,50 @@ def create_tf_record_from_annotations(in_dir, in_ids, out_path):
         if FLAGS.show_debug:
             debug_draw = ImageDraw.Draw(randomised_image)
 
-        for k in annotation:
+        card_rotated_center = None
+        for k in sorted(annotation, key=category_name_to_id, reverse=True):
             if k != "uuid":
-                # All coordinates are normalized here, too
-                c1 = (((annotation[k]["startX"] * card_resize_factor[0]) + card_transform[0]) / randomised_image.size[0], ((annotation[k]["startY"] * card_resize_factor[1]) + card_transform[1]) / randomised_image.size[1])
-                c2 = (((annotation[k]["endX"] * card_resize_factor[0]) + card_transform[0]) / randomised_image.size[0], ((annotation[k]["endY"] * card_resize_factor[1]) + card_transform[1]) / randomised_image.size[1])
+                start = (annotation[k]["startX"], annotation[k]["startY"])
+                end = (annotation[k]["endX"], annotation[k]["endY"])
 
-                if c1 > c2:
-                    c1, c2 = c2, c1 # Swap the variables
-                    print("Found disordered bounding box; it will be swapped.")
+                if tuple(start) > tuple(end):
+                    start, end = end, start # Swap the variables
+
+                start *= np.array(thumbnail_resize_factor)
+
+                if k != "card":
+                    start += content_transform
+
+                bb_size = np.array((math.fabs(annotation[k]["endX"] - annotation[k]["startX"]), math.fabs(annotation[k]["endY"] - annotation[k]["startY"])))*thumbnail_resize_factor
+                rotated_size = get_rotated_bounding_box_size(bb_size, rotation)
+
+                if k == "card":
+                    rotated_center = start + (np.array(rotated_size) / 2)
+                    card_rotated_center = rotated_center # We've sorted the dict, so this happens first
+                else:
+                    rotated_center = (bb_size/2) + start
+
+                rotated_center = rotate_point(rotated_center, card_rotated_center, rotation)
+
+                start = rotated_center-(np.array(rotated_size)/2)
+                end = rotated_center+(np.array(rotated_size)/2)
+
+                start += card_transform
+                end += card_transform
 
                 if FLAGS.show_debug:
-                    debug_draw.rectangle([c1[0] * randomised_image.size[0], c1[1] * randomised_image.size[1], c2[0] * randomised_image.size[0], c2[1] * randomised_image.size[1]])
+                    debug_draw.rectangle([tuple(start), tuple(end)])
+                    for i in xrange(360):
+                        debug_draw.point(tuple(np.array(rotate_point(rotated_center, card_rotated_center, i)) + card_transform))
 
-                startx.append(c1[0])
-                starty.append(c1[1])
-                endx.append(c2[0])
-                endy.append(c2[1])
+                # Normalize coordinates
+                start /= randomised_image.size
+                end /= randomised_image.size
+
+                startx.append(start[0])
+                starty.append(start[1])
+                endx.append(end[0])
+                endy.append(end[1])
 
                 category_id.append(category_name_to_id(k))
                 category_name.append(k.encode("utf8"))
@@ -125,7 +174,7 @@ def create_tf_record_from_annotations(in_dir, in_ids, out_path):
             raw_input("Press any key to show next image.")
             continue
 
-        # This is wildly inefficient... I haven't found a better way to get this
+        # This is wildly inefficient, but I haven't found a better way to get this
         # into a format TensorFlow wants.
         randomised_image_bytes = io.BytesIO()
         randomised_image.save(randomised_image_bytes, format="PNG")
@@ -157,7 +206,8 @@ def category_name_to_id(name):
         "setSymbol": 2,
         "collectorNumber": 3,
         "typeLine": 4,
-        "card": 5
+        "card": 5,
+        "uuid": 1000, # This has no equivalent in the label map; we make it big for sorting
     }[name]
 
 def main(_):
@@ -169,6 +219,9 @@ def main(_):
     train_output_path = os.path.join(FLAGS.output_dir, 'mtg_train.record')
     val_output_path = os.path.join(FLAGS.output_dir, 'mtg_val.record')
     # testdev_output_path = os.path.join(FLAGS.output_dir, 'coco_testdev.record')
+
+    if FLAGS.random_seed != "":
+        random.seed(FLAGS.random_seed)
 
     files = glob.glob(FLAGS.data_dir+"*.json")
     ids = [[], [], []]
