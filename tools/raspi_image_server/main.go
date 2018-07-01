0f423b99ee9633206becd577ca51003d80dd7ebd
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2018-06-30 19:41:04 -0700

    Hack together a raspi over http solution.

--- tools/raspi_image_server/main.go
+++ tools/raspi_image_server/main.go
@@ -5,20 +5,56 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"sync"
+	"time"
 
 	"github.com/ogier/pflag"
 )
+
+var cameraMux sync.Mutex
 
 func main() {
 	bind := pflag.StringP("bind", "b", "localhost:8000", "Address to run the http server on.")
 	pflag.Parse()
 
-	http.HandleFunc("/", httpHandler)
+	srv := &http.Server{
+		WriteTimeout: 8 * time.Second,
+		ReadTimeout:  8 * time.Second,
+		Addr:         *bind,
+		Handler:      handler{},
+	}
+
 	log.Println("Listening on", *bind)
-	log.Panic(http.ListenAndServe(*bind, nil))
+	log.Panic(srv.ListenAndServe())
 }
 
-func httpHandler(w http.ResponseWriter, req *http.Request) {
-	cmd := exec.Command("raspistill", "-o", "-")
-	io.Copy(w, cmd.Stdin)
+type handler struct{}
+
+func (handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	cameraMux.Lock()
+	cmd := exec.Command("raspistill", "-o", "-", "-t", "1", "-n", "-e", "jpg")
+
+	stdout, err := cmd.StdoutPipe()
+	defer stdout.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		cmd.Process.Kill()
+		return
+	}
+
+	if err := cmd.Start(); err != nil {
+		http.Error(w, err.Error(), 500)
+		cmd.Process.Kill()
+		return
+	}
+
+	go func() {
+		io.Copy(w, stdout) // raspistill doesn't ever return an EOF, so this is our nasty little hack around that
+	}()
+
+	if err := cmd.Wait(); err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+	log.Println("unlocking")
+	cameraMux.Unlock()
 }
