dcb00c01bb3fac6d4d9de2458a5863d83abbcd9d
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2018-09-04 23:04:55 -0700

    Support no data augmentation in the tfrecord generator.

--- tools/tfrecord_generator/generate.py
+++ tools/tfrecord_generator/generate.py
@@ -29,6 +29,7 @@ tf.flags.DEFINE_string('data_dir', './raw_data/',
                        'Training image directory.')
 tf.flags.DEFINE_string('output_dir', '/tmp/', 'Output data directory.')
 tf.flags.DEFINE_boolean('show_debug', False, 'Display a debug image with bounding boxes drawn.')
+tf.flags.DEFINE_boolean('augment', True, 'Augment card images by (randomly) compositing them on a background, applying transformations, and other image filtering. This should be disabled for "real" images.')
 tf.flags.DEFINE_string('random_seed', '', 'Seed to supply to the random number generator.')
 
 FLAGS = flags.FLAGS
@@ -42,11 +43,17 @@ MAX_ROTATION_DEGREES = 8
 MAX_BLUR_RADIUS = 1.3
 FLIPPED_PROBABILITY = 0.6
 
+background_cache = {}
+
 tf.logging.set_verbosity(tf.logging.INFO)
 
 def randomise_image(foreground_bytes, background_path):
     im = Image.open(foreground_bytes)
-    bg = Image.open(background_path, mode="r")
+    if not background_path in background_cache:
+        bg = Image.open(background_path, mode="r")
+        background_cache[background_path] = bg
+    else:
+        bg = background_cache[background_path]
 
     origsize = im.size
 
@@ -102,8 +109,6 @@ def create_tf_record_from_annotations(in_dir, in_ids, out_path):
     writer = tf.python_io.TFRecordWriter(out_path)
 
     for v in in_ids:
-        background_png_path = os.path.join(in_dir, random.choice(BACKGROUND_NAMES))
-
         json_path = os.path.join(in_dir, v + ".json")
         with tf.gfile.GFile(json_path, "r") as f:
             json_raw = f.read()
@@ -112,10 +117,16 @@ def create_tf_record_from_annotations(in_dir, in_ids, out_path):
         filename = v + ".png"
         card_png_path = os.path.join(in_dir, filename)
         print(card_png_path)
-        with tf.gfile.GFile(card_png_path, 'rb') as fid:
-            encoded_png = fid.read()
-        randomised_image, card_transform, thumbnail_resize_factor, card_original_size, rotation, content_transform = randomise_image(io.BytesIO(encoded_png), background_png_path)
-        if randomised_image.format != "PNG":
+
+        if FLAGS.augment:
+            background_png_path = os.path.join(in_dir, random.choice(BACKGROUND_NAMES))
+            with tf.gfile.GFile(card_png_path, 'rb') as fid:
+                encoded_png = fid.read()
+            card_image, card_transform, thumbnail_resize_factor, card_original_size, rotation, content_transform = randomise_image(io.BytesIO(encoded_png), background_png_path)
+        else:
+            card_image = Image.open(card_png_path)
+            card_original_size = card_image.size
+        if card_image.format != "PNG":
             raise ValueError("Image format not PNG")
 
         # Simulated annotation data doesn't include the card bounding box
@@ -135,47 +146,51 @@ def create_tf_record_from_annotations(in_dir, in_ids, out_path):
         category_name = []
 
         if FLAGS.show_debug:
-            debug_draw = ImageDraw.Draw(randomised_image)
+            debug_draw = ImageDraw.Draw(card_image)
 
         card_rotated_center = None
         for k in sorted(annotation, key=category_name_to_id, reverse=True):
+            if k == "upsideDown":
+                continue # This attribute is currently unused, it's just there in case we need it
+
             if k != "uuid":
-                start = (annotation[k]["startX"], annotation[k]["startY"])
-                end = (annotation[k]["endX"], annotation[k]["endY"])
+                start = annotation[k]["startX"], annotation[k]["startY"]
+                end = annotation[k]["endX"], annotation[k]["endY"]
 
                 if tuple(start) > tuple(end):
                     start, end = end, start # Swap the variables
 
-                start *= np.array(thumbnail_resize_factor)
+                if FLAGS.augment:
+                    start *= np.array(thumbnail_resize_factor)
+                    if k != "card":
+                        start += content_transform
 
-                if k != "card":
-                    start += content_transform
+                    bb_size = np.array((math.fabs(annotation[k]["endX"] - annotation[k]["startX"]), math.fabs(annotation[k]["endY"] - annotation[k]["startY"])))*thumbnail_resize_factor
+                    rotated_size = get_rotated_bounding_box_size(bb_size, rotation)
 
-                bb_size = np.array((math.fabs(annotation[k]["endX"] - annotation[k]["startX"]), math.fabs(annotation[k]["endY"] - annotation[k]["startY"])))*thumbnail_resize_factor
-                rotated_size = get_rotated_bounding_box_size(bb_size, rotation)
+                    if k == "card":
+                        rotated_center = start + (np.array(rotated_size) / 2)
+                        card_rotated_center = rotated_center # We've sorted the dict, so this happens first
+                    else:
+                        rotated_center = (bb_size/2) + start
 
-                if k == "card":
-                    rotated_center = start + (np.array(rotated_size) / 2)
-                    card_rotated_center = rotated_center # We've sorted the dict, so this happens first
-                else:
-                    rotated_center = (bb_size/2) + start
+                    rotated_center = rotate_point(rotated_center, card_rotated_center, rotation)
 
-                rotated_center = rotate_point(rotated_center, card_rotated_center, rotation)
+                    start = rotated_center-(np.array(rotated_size)/2)
+                    end = rotated_center+(np.array(rotated_size)/2)
 
-                start = rotated_center-(np.array(rotated_size)/2)
-                end = rotated_center+(np.array(rotated_size)/2)
-
-                start += card_transform
-                end += card_transform
+                    start += card_transform
+                    end += card_transform
 
                 if FLAGS.show_debug:
                     debug_draw.rectangle([tuple(start), tuple(end)])
-                    for i in xrange(360):
-                        debug_draw.point(tuple(np.array(rotate_point(rotated_center, card_rotated_center, i)) + card_transform))
+                    if FLAGS.augment:
+                        for i in xrange(360):
+                            debug_draw.point(tuple(np.array(rotate_point(rotated_center, card_rotated_center, i)) + card_transform))
 
                 # Normalize coordinates
-                start /= randomised_image.size
-                end /= randomised_image.size
+                start /= np.array(card_image.size)
+                end /= np.array(card_image.size)
 
                 startx.append(start[0])
                 starty.append(start[1])
@@ -186,22 +201,22 @@ def create_tf_record_from_annotations(in_dir, in_ids, out_path):
                 category_name.append(k.encode("utf8"))
 
         if FLAGS.show_debug:
-            randomised_image.show()
+            card_image.show()
             raw_input("Press any key to show next image.")
             continue
 
         # This is wildly inefficient, but I haven't found a better way to get this
         # into a format TensorFlow wants.
-        randomised_image_bytes = io.BytesIO()
-        randomised_image.save(randomised_image_bytes, format="PNG")
+        card_image_bytes = io.BytesIO()
+        card_image.save(card_image_bytes, format="PNG")
 
         example = tf.train.Example(features=tf.train.Features(feature={
-            "image/width": dataset_util.int64_feature(randomised_image.size[0]),
-            "image/height": dataset_util.int64_feature(randomised_image.size[1]),
+            "image/width": dataset_util.int64_feature(card_image.size[0]),
+            "image/height": dataset_util.int64_feature(card_image.size[1]),
             "image/filename": dataset_util.bytes_feature(filename.encode("utf8")),
             "image/source_id": dataset_util.bytes_feature(
                 annotation["uuid"].encode("utf8")),
-            "image/encoded": dataset_util.bytes_feature(randomised_image_bytes.getvalue()),
+            "image/encoded": dataset_util.bytes_feature(card_image_bytes.getvalue()),
             "image/format": dataset_util.bytes_feature(b"png"),
             "image/object/bbox/xmin": dataset_util.float_list_feature(startx),
             "image/object/bbox/xmax": dataset_util.float_list_feature(endx),
@@ -224,6 +239,7 @@ def category_name_to_id(name):
         "typeLine": 4,
         "card": 5,
         "uuid": 1000, # This has no equivalent in the label map; we make it big for sorting
+        "upsideDown": 1001, # Same as above
     }[name]
 
 def main(_):
@@ -234,7 +250,6 @@ def main(_):
         tf.gfile.MakeDirs(FLAGS.output_dir)
     train_output_path = os.path.join(FLAGS.output_dir, 'mtg_train.record')
     val_output_path = os.path.join(FLAGS.output_dir, 'mtg_val.record')
-    # testdev_output_path = os.path.join(FLAGS.output_dir, 'coco_testdev.record')
 
     if FLAGS.random_seed != "":
         random.seed(FLAGS.random_seed)
@@ -259,7 +274,6 @@ def main(_):
 
     create_tf_record_from_annotations(FLAGS.data_dir, ids[0], train_output_path)
     create_tf_record_from_annotations(FLAGS.data_dir, ids[1], val_output_path)
-    # create_tf_record_from_annotations(FLAGS.data_dir, ids[2], testdev_output_path)
 
 if __name__ == '__main__':
     tf.app.run()
